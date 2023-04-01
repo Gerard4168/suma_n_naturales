16b01300c007a2492a68062465c4bd136fe2b1dd
Gerard4168 <gerard2234506@correo.uis.edu.co>
2023-04-01 07:49:28 -0500

    Subir

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # suma_n_naturales
+
+# Hallar la suma de la sucecion de numeros
+
+# bucles
---
+++ proceso.py
@@ -0,0 +1,40 @@
+# Bucles
+
+# contadores , sumadores , acumuladores 
+
+n=int(input("Digite el valor de n: "))
+
+i=1
+suma=0
+
+while (i<=n):
+    suma=suma+i
+    i=i+1
+    print("I = "+str(i))
+    print("La suma es: "+str(suma))
+print("La suma es: "+str(suma))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
